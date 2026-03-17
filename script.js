5f293bba0a638f0bb5f2334496281804ea4df5e6
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-17 11:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for name
+  set name(name) {
+    this._name = name;
+  }
+
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
